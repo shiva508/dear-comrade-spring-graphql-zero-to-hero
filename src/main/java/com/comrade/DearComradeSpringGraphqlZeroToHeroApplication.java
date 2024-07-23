a4a879e6225f1d96c5163f2562fdd5d4e364107d
shiva508 <dasarishiva1@gmail.com>
2024-07-23 21:48:19 +0530

    first commit

---
+++ src/main/java/com/comrade/DearComradeSpringGraphqlZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearComradeSpringGraphqlZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearComradeSpringGraphqlZeroToHeroApplication.class, args);
+	}
+
+}
